313ada9c2ba10cab9dc7a644c7441a2e0a58f159
Prince-i <prince_arce01@outlook.com>
2021-05-12 16:51:50 +0800

    TUBEMAKING

--- Component/Modals/new_plan.php
+++ Component/Modals/new_plan.php
@@ -1,4 +1,4 @@
-<div class="modal bottom-sheet" id="create-plan" style="max-height:100vh;">
+<div class="modal bottom-sheet" id="create-plan" style="min-height:90vh;">
     <div class="modal-footer">
         <button class="modal-close btn-flat" style="font-size:30px;color:#f55353;">
             <b>&times;</b>
